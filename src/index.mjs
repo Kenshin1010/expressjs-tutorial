ee8c35d5baed7fe2aeaa89a63dfad57fe999c7d1
anh.do <tuananh9h@gmail.com>
2024-06-12 15:48:15 +0700

    PUT Requests

--- src/index.mjs
+++ src/index.mjs
@@ -61,6 +61,27 @@ app.get("/api/products", (request, response) => {
   response.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
 });
 
+app.put("/api/users/:id", (request, response) => {
+  const {
+    body,
+    params: { id },
+  } = request;
+
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return response.sendStatus(400);
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+
+  if (findUserIndex === -1) return response.sendStatus(404);
+
+  mockUsers[findUserIndex] = { id: parsedId, ...body };
+  return response.sendStatus(200);
+});
+
 // localhost:3000
 // localhost:3000/users
 // localhost:3000/products?key=value&key2=value2
+
+// PUT
+// PATCH
+// DELETE
