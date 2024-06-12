210139bd788832da9dbd27db3cce5f507ad7ecf2
anh.do <tuananh9h@gmail.com>
2024-06-12 16:06:35 +0700

    PATCH Requests

--- src/index.mjs
+++ src/index.mjs
@@ -78,6 +78,23 @@ app.put("/api/users/:id", (request, response) => {
   return response.sendStatus(200);
 });
 
+app.patch("/api/users/:id", (request, response) => {
+  const {
+    body,
+    params: { id },
+  } = request;
+
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return response.sendStatus(400);
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+
+  if (findUserIndex === -1) return response.sendStatus(404);
+
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+  return response.sendStatus(200);
+});
+
 // localhost:3000
 // localhost:3000/users
 // localhost:3000/products?key=value&key2=value2
