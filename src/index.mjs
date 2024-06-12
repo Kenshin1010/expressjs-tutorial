000761cdfdde0a413427368bbe0fa0193c4a7388
anh.do <tuananh9h@gmail.com>
2024-06-12 16:31:07 +0700

    DELETE Requests

--- src/index.mjs
+++ src/index.mjs
@@ -95,6 +95,20 @@ app.patch("/api/users/:id", (request, response) => {
   return response.sendStatus(200);
 });
 
+app.delete("/api/users/:id", (request, response) => {
+  const {
+    params: { id },
+  } = request;
+
+  const parsedId = parseInt(id);
+
+  if (isNaN(parsedId)) return response.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return response.sendStatus(404);
+  mockUsers.splice(findUserIndex, 1);
+  return response.sendStatus(200);
+});
+
 // localhost:3000
 // localhost:3000/users
 // localhost:3000/products?key=value&key2=value2
