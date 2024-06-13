48250f227b6252084fcdfe774b0037ec042e6caf
anh.do <tuananh9h@gmail.com>
2024-06-13 18:04:07 +0700

    Hashing Passwords

--- src/routes/users.mjs
+++ src/routes/users.mjs
@@ -9,6 +9,7 @@ import { mockUsers } from "../utils/constants.mjs";
 import { createUserValidationSchema } from "../utils/validationSchemas.mjs";
 import { resolveIndexByUserId } from "../utils/middlewares.mjs";
 import { User } from "../mongoose/schemas/user.mjs";
+import { hashPassword } from "../utils/helpers.mjs";
 
 const router = Router();
 
@@ -59,6 +60,11 @@ router.post(
     if (!result.isEmpty()) return response.status(400).send(result.array());
 
     const data = matchedData(request);
+
+    console.log(data);
+    data.password = hashPassword(data.password);
+    console.log(data);
+
     const newUser = new User(data);
     try {
       const saveUser = await newUser.save();
