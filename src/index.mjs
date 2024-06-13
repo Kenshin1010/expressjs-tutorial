926f6e6d58e5d7f222e4ea1d29d3ae49837ddb4e
anh.do <tuananh9h@gmail.com>
2024-06-13 10:52:49 +0700

    Sessions Pt.1

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,22 @@
 import express from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser("helloworld"));
+app.use(
+  session({
+    secret: "anson the dev",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60
+    }
+  })
+);
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -15,6 +26,8 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (request, response) => {
+  console.log(request.session.id);
+  request.session.visited = true;
   response.cookie("hello", "world", { maxAge: 10000, signed: true });
   response.status(201).send({ msg: "Hello" });
 });
