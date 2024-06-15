df37e35209bb7e7b169c82f996fb71a02eb0bfb8
anh.do <tuananh9h@gmail.com>
2024-06-15 11:26:15 +0700

    split route auth createApp

--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -3,6 +3,37 @@ import { Router } from "express";
 
 const router = Router();
 
+router.post(
+  "/api/auth",
+  passport.authenticate("local"),
+  (request, response) => {
+    response.sendStatus(200);
+  }
+);
+
+router.get("/api/auth/status", (request, response) => {
+  return request.user ? response.send(request.user) : response.sendStatus(401);
+});
+
+router.post("/api/auth/logout", (request, response) => {
+  if (!request.user) return response.sendStatus(401);
+  request.logout((err) => {
+    if (err) return response.sendStatus(400);
+    response.send(200);
+  });
+});
+
+router.get("/api/auth/discord", passport.authenticate("discord"));
+router.get(
+  "/api/auth/discord/redirect",
+  passport.authenticate("discord"),
+  (request, response) => {
+    console.log(request.session);
+    console.log(request.user);
+    response.sendStatus(200);
+  }
+);
+
 // app.get("/", (request, response) => {
 //   console.log(request.session.id);
 //   request.session.visited = true;
