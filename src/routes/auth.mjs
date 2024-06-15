9ca9b9413e91531823f0e1cdd2c47702a3f84769
anh.do <tuananh9h@gmail.com>
2024-06-15 11:29:38 +0700

    test cartRouter

--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -36,47 +36,47 @@ router.get(
 
 export default router;
 
-// app.get("/", (request, response) => {
-//   console.log(request.session.id);
-//   request.session.visited = true;
-//   response.cookie("hello", "world", { maxAge: 30000, signed: true });
-//   response.status(201).send({ msg: "Hello" });
-// });
+router.get("/", (request, response) => {
+  console.log(request.session.id);
+  request.session.visited = true;
+  response.cookie("hello", "world", { maxAge: 30000, signed: true });
+  response.status(201).send({ msg: "Hello" });
+});
 
-// app.post("/api/auth", (request, response) => {
-//   const {
-//     body: { username, password }
-//   } = request;
-//   const findUser = mockUsers.find((user) => user.username === username);
-//   if (!findUser || findUser.password !== password)
-//     return response.status(401).send({ msg: "BAD CREDENTIALS" });
+router.post("/api/auth", (request, response) => {
+  const {
+    body: { username, password }
+  } = request;
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password)
+    return response.status(401).send({ msg: "BAD CREDENTIALS" });
 
-//   request.session.user = findUser;
-//   return response.status(200).send(findUser);
-// });
+  request.session.user = findUser;
+  return response.status(200).send(findUser);
+});
 
-// app.get("/api/auth/status", (request, response) => {
-//   request.sessionStore.get(request.sessionID, (err, session) => {
-//     console.log(session);
-//   });
-//   return request.session.user
-//     ? response.status(200).send(request.session.user)
-//     : response.status(401).send({ msg: "Not Authenticated" });
-// });
+router.get("/api/auth/status", (request, response) => {
+  request.sessionStore.get(request.sessionID, (err, session) => {
+    console.log(session);
+  });
+  return request.session.user
+    ? response.status(200).send(request.session.user)
+    : response.status(401).send({ msg: "Not Authenticated" });
+});
 
-// app.post("/api/cart", (request, response) => {
-//   if (!request.session.user) return response.sendStatus(401);
-//   const { body: item } = request;
-//   const { cart } = request.session;
-//   if (cart) {
-//     cart.push(item);
-//   } else {
-//     request.session.cart = [item];
-//   }
-//   return response.status(201).send(item);
-// });
+router.post("/api/cart", (request, response) => {
+  if (!request.session.user) return response.sendStatus(401);
+  const { body: item } = request;
+  const { cart } = request.session;
+  if (cart) {
+    cart.push(item);
+  } else {
+    request.session.cart = [item];
+  }
+  return response.status(201).send(item);
+});
 
-// app.get("/api/cart", (request, response) => {
-//   if (!request.session.user) return response.sendStatus(401);
-//   return response.send(request.session.cart ?? []);
-// });
+router.get("/api/cart", (request, response) => {
+  if (!request.session.user) return response.sendStatus(401);
+  return response.send(request.session.cart ?? []);
+});
